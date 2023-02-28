ab119635fb569b4a6d874efcbf9227b1375ef7d2
Kanwar Pannu <kanwarsnake@gmail.com>
2023-02-28 00:58:40 -0500

    Initial commit

---
+++ src/test/java/com/poc/graphqlserver/GraphqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.graphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
