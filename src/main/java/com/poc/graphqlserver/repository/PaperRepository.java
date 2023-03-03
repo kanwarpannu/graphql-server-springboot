1a705c1fc79b6aa5c5b5a7f6fc138c432be38106
Kanwar Pannu <kanwarsnake@gmail.com>
2023-03-03 00:09:38 -0500

    removed broken graphqldls code

--- src/main/java/com/poc/graphqlserver/repository/PaperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poc.graphqlserver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.graphql.data.GraphQlRepository;
-
-@GraphQlRepository
-public interface PaperRepository extends JpaRepository<Paper, String>, QuerydslPredicateExecutor<Paper> {
-}
